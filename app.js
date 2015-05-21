d43bb1cfd2d24b0c1e595a4b5ee5c4c745523a1f
kkomaz <leeajl006@gmail.com>
2015-05-20 23:01:36 -0400

    added dynamic routing

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var app = express();
 
-var cities = ['Caspiana', 'Indigo', 'Paradise'];
+var cities = {
+  'Lotopia': 'Rough and mountainous',
+  'Caspiana': 'Sky-top island',
+  'Indigo': 'Vibrant and thriving',
+  'Paradise': 'Lush, green plantation',
+  'Flotilla': 'Bustling urban oasis'
+};
 
-app.get('/cities', function(request, response){
-  if (request.query.search){
-    response.json(citySearch(request.query.search));
+app.get('/cities/:name', function (request, response) {
+  var cityInfo;
+  if (cities[request.params.name]){
+    cityInfo = cities[request.params.name];
+    response.json(cityInfo);
+  } else {
+    response.status(404).json("City not found");
   }
 });
-        
-function citySearch (keyword) {
-  var regexp = RegExp(keyword, 'i');
-  var result = cities.filter(function (city) {
-    return city.match(regexp);
-  });
-
-  return result;
-}
 
 app.listen(3000);

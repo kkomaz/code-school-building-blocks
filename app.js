42c6eefedb966f6240e813d52eeefdd5430c7386
kkomaz <leeajl006@gmail.com>
2015-05-21 23:01:55 -0400

    express tutorial complete

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var app = express();
 
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false });
+var router = require('./routes/cities');
 
-app.post('/cities', parseUrlencoded, function (request, response) {
-  if (request.body.description.length > 4){
-    var city = createCity(request.body.name, request.body.description);
-    response.status(201).json(city);
-  } else {
-    response.status(400).json("Invalid City");
-  }
-});
-
+app.use('/cities', router);
 app.listen(3000);
